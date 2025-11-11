f40e528385b76ce7040c559079db13a000c97232
Manasvee16 <manasveerathie@gmail.com>
2025-11-11 21:59:13 +0530

    Retrain model with scikit-learn GradientBoostingRegressor (R=0.9289): removes CatBoost 150MB dependency

--- src/pipelines/Prediction_Pipleline.py
+++ src/pipelines/Prediction_Pipleline.py
@@ -4,41 +4,7 @@ import numpy as np
 from src.logger import logging
 from src.exception import CustomException
 # Make sure your utils.py file is in src/utils/ and has a load_object function
-from src.utils.utils import load_object
-
-# Hardcoded scaling statistics from training data (removes scikit-learn dependency)
-# Extracted from Artifacts/train_data.csv training set
-FEATURE_MEANS = np.array([
-    3.6091246287128715,
-    11.569306930693068,
-    10.985049504950496,
-    0.07178217821782178,
-    0.5564841584158416,
-    6.315891089108911,
-    68.55643564356437,
-    3.8081950495049504,
-    9.356435643564357,
-    404.0321782178218,
-    18.318316831683166,
-    356.27834158415845,
-    12.457351485148516,
-])
-
-FEATURE_STDS = np.array([
-    8.875058224913126,
-    23.152480822861172,
-    6.89461758261083,
-    0.25844695944787016,
-    0.11770447451827175,
-    0.7094517563306172,
-    27.994922140912426,
-    2.1312264279567033,
-    8.589721087186096,
-    166.17265514913421,
-    2.228700928767628,
-    91.56653276201261,
-    7.110381022008366,
-])
+from src.utils.utils import load_object 
 
 class PredictPipeline:
     def __init__(self):
@@ -48,12 +14,12 @@ class PredictPipeline:
         try:
             # Standard folder name is 'artifacts' (lowercase)
             model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
             
             model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
             
-            # Apply standardization using hardcoded training statistics
-            # This replaces the scikit-learn StandardScaler
-            data_scaled = (features - FEATURE_MEANS) / FEATURE_STDS
+            data_scaled = preprocessor.transform(features)
             pred = model.predict(data_scaled)
             return pred
         
